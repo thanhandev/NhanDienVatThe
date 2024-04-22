c69dda876b27be9cd5fbcdaaff4ba828d522450f
Thành An <83403597+thanhandev@users.noreply.github.com>
2024-04-22 12:00:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source bin/activate
 
 pip3 install --upgrade pip
 sudo pip3 install --upgrade setuptools
+sudo apt-get install python3-dev
 sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
 sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get install libatlas-base-dev
@@ -17,7 +18,7 @@ sudo apt-get install libhdf5-dev libc-ares-dev libeigen3-dev
 sudo apt-get install libatlas-base-dev libopenblas-dev libblas-dev
 sudo apt-get install liblapack-dev cython
 sudo pip3 install pybind11
-sudo pip3 install h5py
+sudo pip3 install h5py==3.10
 pip install tensorflow
 
 pip install matplotlib
